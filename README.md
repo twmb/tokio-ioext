05ed7646d42a15089ddcfcc1e82ffbe64032dbbb
Travis Bischel <twmb@lavabit.com>
2018-02-09 19:37:11 -0800

    rename crate to tokio-ioext

--- README.md
+++ README.md
@@ -1,8 +1,6 @@
-tokio-try-read-full
-===================
+tokio-ioext
+===========
 
- [![Crates.io](https://img.shields.io/crates/v/tokio-try-read-full.svg)](https://crates.io/crates/tokio-try-read-full) [![Documentation](https://docs.rs/tokio-try-read-full/badge.svg)](https://docs.rs/tokio-try-read-full/)
+ [![Crates.io](https://img.shields.io/crates/v/tokio-ioext.svg)](https://crates.io/crates/tokio-ioext) [![Documentation](https://docs.rs/tokio-ioext/badge.svg)](https://docs.rs/tokio-ioext/)
 
-This crate complemnts `tokio-io`'s `read_exact` for scenarios where you want to
-read as many bytes as possible into a buffer until either the buffer is full or
-the reader returns `EOF`.
+This crate contains more niche functions that complement or extend `tokio-io`.
--- src/lib.rs
+++ src/lib.rs
@@ -1,83 +1,13 @@
-//! This library provides a future to complement `tokio-io`'s `read_exact`. Sometimes, you want to
-//! read as much as possible into a large buffer. It does not have to be an error to not fill the
-//! buffer.
-use std::io;
-use std::mem;
+//! This library provides functions that are similar but not equal to `tokio-io` functions.
+//!
+//! These functions can be more specialized or provide more control for performing asynchronous io.
 
 extern crate futures;
 #[macro_use]
 extern crate tokio_io;
 
-use futures::{Future, Poll};
-use tokio_io::AsyncRead;
+mod try_read_full;
+mod read_lim;
 
-/// A future which can be used to read as many bytes as possible into a buffer.
-///
-/// Created by the [`try_read_full`] function.
-///
-/// [`try_read_full`]: fn.try_read_full.html
-#[derive(Debug)]
-pub struct TryReadFull<A, T> {
-    state: State<A, T>,
-}
-
-#[derive(Debug)]
-enum State<A, T> {
-    Reading { a: A, buf: T, pos: usize },
-    Empty,
-}
-
-/// Creates a future which will read as many bytes as possible into `buf` and return the number of
-/// bytes read.
-///
-/// The returned future will resolve to the I/O stream, the buffer, and the total bytes read once
-/// the read operation is completed.
-///
-/// In the case of an error the buffer and the object will be discarded, with the error yielded.
-pub fn try_read_full<A, T>(a: A, buf: T) -> TryReadFull<A, T>
-where
-    A: AsyncRead,
-    T: AsMut<[u8]>,
-{
-    TryReadFull {
-        state: State::Reading {
-            a: a,
-            buf: buf,
-            pos: 0,
-        },
-    }
-}
-
-impl<A, T> Future for TryReadFull<A, T>
-where
-    A: AsyncRead,
-    T: AsMut<[u8]>,
-{
-    type Item = (A, T, usize);
-    type Error = io::Error;
-
-    fn poll(&mut self) -> Poll<(A, T, usize), io::Error> {
-        match self.state {
-            State::Reading {
-                ref mut a,
-                ref mut buf,
-                ref mut pos,
-            } => {
-                let buf = buf.as_mut();
-                while *pos < buf.len() {
-                    let n = try_nb!(a.read(&mut buf[*pos..]));
-                    *pos += n;
-                    if n == 0 {
-                        break;
-                    }
-                }
-            }
-            State::Empty => panic!("poll a TryReadFull after it's done"),
-        }
-
-        match mem::replace(&mut self.state, State::Empty) {
-            State::Reading { a, buf, pos } => Ok((a, buf, pos).into()),
-            State::Empty => panic!(),
-        }
-    }
-}
+pub use try_read_full::*;
+pub use read_lim::*;
